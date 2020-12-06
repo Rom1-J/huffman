bd6c185118e1db28d211a1e88b7dcbe8df3b0c5c
Romain J <romain.ordi@gmail.com>
2020-12-06 23:24:17 +0100

    feat(utils): done part1 func2 count chars

---
+++ inc/utils.h
@@ -0,0 +1,12 @@
+//
+// Created by romain on 06/12/2020.
+//
+
+#include <stdio.h>
+
+#ifndef INC_HUFFMAN_UTILS_H
+#define INC_HUFFMAN_UTILS_H
+
+void show_chars_count(FILE *fpi);
+
+#endif //INC_HUFFMAN_UTILS_H
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "../inc/translate.h"
+#include "../inc/utils.h"
 #include <stdio.h>
 
 int main(void) {
@@ -8,6 +9,9 @@ int main(void) {
     fpo = fopen("test/Output.txt", "w");
 
     translate_file(fpi, fpo);
+
+    fpi = fopen("test/Alice.txt", "r");
+    show_chars_count(fpi);
 
     return 0;
 }
